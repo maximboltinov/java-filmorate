0ec093657af8f3f847194799177c1b7d57fdb3d3
maximboltinov <maxim.boltinov@yandex.com>
2023-11-08 03:39:46 +0300

    spaces

--- src/main/java/ru/yandex/practicum/filmorate/exception/NoUserException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NoUserException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class NoUserException extends RuntimeException{
+public class NoUserException extends RuntimeException {
     public NoUserException(String message) {
         super(message);
     }
