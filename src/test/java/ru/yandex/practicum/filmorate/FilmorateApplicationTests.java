076afd933101c6910a816ffbc75b2cb5ca33689c
maximboltinov <maxim.boltinov@yandex.com>
2023-11-08 03:43:13 +0300

    unused imports

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.controller.UserController;
 
 @SpringBootTest
 class FilmorateApplicationTests {
